b349e9c3b809e5485b67dae8b8fd51277e471889
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-03-03 23:56:21 +0100

    Feature: Add Top Bar

---
+++ src/components/TopNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="top-bar-bg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+}
+</script>
+
+<style scoped lang="scss">
+.top-bar-bg {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+  width: 100vw;
+  background-color: #dde2e9;
+}
+
+</style>
